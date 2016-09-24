6e9a99378ddf560a9bc5f49a5192bbd866aa7edb
sashwa <sashwa@users.noreply.github.com>
2016-09-24 11:18:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article', function(req, res){
+app.get('/article-one', function(req, res){
     res.send('Article -1 requested and serrved here');
 });
-app.get('article-two', function(req, res){
+app.get('/article-two', function(req, res){
     res.send('Article -2 requested and serrved here');
 });
-app.get('article-three', function(req, res){
+app.get('/article-three', function(req, res){
     res.send('Article -3 requested and serrved here');
 });
 app.get('/ui/style.css', function (req, res) {
