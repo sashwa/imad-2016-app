91d098454f83c41217e9e1b0b489003845e31960
sashwa <sashwa@users.noreply.github.com>
2016-09-28 17:54:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,25 @@
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onClick=function(){
+  //Creta Make a request
+  var request=new XMLhttpRequest();
   
-    counter =counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
+  //capture response and storee in variable
+  request.onreadystatechange=function(){
+      if(request.readystate===XMLhttpRequest.DONE){
+          //action required
+          if(request.status===200){
+           var counter=request.responseText;
+            var span = document.getElementById('count');
+
+           span.innerHTML=counter.toString();
+          }
+      }
+  };
+  
+  //Make a request
+  request.open('GET','http://sashwa.imad.hasura-app.io/counter',true);
+  request.send(null);
+  
     
 };
